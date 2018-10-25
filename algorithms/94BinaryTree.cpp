c3dde64ab91a9b334153a91c01369cc3f93fa1bc
Qiuai Fu <fuqiuaihappy@163.com>
2018-10-25 16:03:41 +0800

    Create 94BinaryTree.cpp

---
+++ algorithms/94BinaryTree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    vector<int> res;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        //采用递归，深度优先遍历
+        if (root){
+            inorderTraversal(root->left);
+            res.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return res;
+    }
+};
